f8c6259121defca3702d34953aa4bd8c2ed9e214
Daniel Guillan <daniel.guillan@gmail.com>
2015-03-24 15:31:20 +0100

    Fix some copy and paste

--- lib/bem-constructor.rb
+++ lib/bem-constructor.rb
@@ -2,7 +2,7 @@ require 'compass'
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('bem-constructor', :path => extension_path)
 
-module QuantityQueries
+module BEMConstructor
   VERSION = "0.1"
   DATE = "2015-03-24"
 end
