e12213bed006120a0806134e4223185e35376f5e
Daniel Guillan <daniel.guillan@gmail.com>
2016-02-07 20:16:20 +0100

    Bump version 1.0.0

--- lib/bem-constructor.rb
+++ lib/bem-constructor.rb
@@ -3,6 +3,6 @@ extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('bem-constructor', :path => extension_path)
 
 module BEMConstructor
-  VERSION = "0.9.0"
-  DATE = "2015-03-26"
+  VERSION = "1.0.0"
+  DATE = "2016-02-07"
 end
